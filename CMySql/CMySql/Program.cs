ce213dfb315e161c4169f4a1a931af0a29e9e732
rafaelnunyez <rafa97burja@gmail.com>
2018-09-14 10:28:45 +0200

    CMySql implementada lectura de la tabla categoria

--- CMySql/CMySql/Program.cs
+++ CMySql/CMySql/Program.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
 namespace CMySql
 {
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-			MySqlConnection mySqlConnection = new MySqlConnection(
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			IDbConnection dbConnection = new MySqlConnection(
 				"server=localhost;database=dbprueba;user=root;password=sistemas;ssl-mode=none"
 			);
-            mySqlConnection.Open();
-			mySqlConnection.Close();
-        }
-    }
+			dbConnection.Open();
+
+			IDbCommand dbCommand = dbConnection.CreateCommand();
+			dbCommand.CommandText = "select * from categoria order by id";
+			IDataReader dataReader = dbCommand.ExecuteReader();
+			dataReader.
+			while (dataReader.Read())
+				Console.WriteLine("id='{0}' nombre='{1}'", dataReader["id"], dataReader["nombre"]);
+			//Console.WriteLine("id='{0}' nombre='{1}'", datareader[0], datareader[1]);
+			dataReader.Close();
+
+			dbConnection.Close();
+		}
+
+		private static String[] getFieldNames (IDataReader dataReader) {
+            int i = 0
+			String[] vs = new string[];
+			while (dataReader.GetName)
+				vs[i] = dataReader.GetName
+		}
+	}
 }
