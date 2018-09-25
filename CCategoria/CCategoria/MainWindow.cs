cb34785c577405818ecb37b0af3bc45db021eddf
rafaelnunyez <rafa97burja@gmail.com>
2018-09-25 09:17:48 +0200

    CCategoria: pruebas de insert update y delete

--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -7,16 +7,22 @@ using CCategoria;
 
 using System.Reflection;
 
-public partial class MainWindow : Gtk.Window
-{
-    public MainWindow() : base(Gtk.WindowType.Toplevel)
-    {
+public partial class MainWindow : Gtk.Window {
+
+	private IDbConnection dbConnection;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel) {
         Build();
 
-		IDbConnection dbConnection = new MySqlConnection(
+		dbConnection = new MySqlConnection(
                 "server=localhost;database=dbprueba;user=root;password=sistemas;ssl-mode=none"
             );
         dbConnection.Open();
+
+		//insert();
+		//update();
+		update(new Categoria(3, "categoria 3 " + DateTime.Now));
+		//delete();
 
 		IDbCommand dbCommand = dbConnection.CreateCommand();
         dbCommand.CommandText = "select id, nombre from categoria order by id";
@@ -90,8 +96,45 @@ public partial class MainWindow : Gtk.Window
         dbConnection.Close();
     }
 
-    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
-    {
+	private void insert() {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = "insert into categoria (nombre) values ('categoria 4')";
+
+        dbCommand.ExecuteNonQuery();
+    }
+
+	private void update() {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = "update categoria set nombre='categoria 4 modificada' where id=4";
+
+        dbCommand.ExecuteNonQuery();
+    }
+
+	private void update(Categoria categoria) {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+		dbCommand.CommandText = string.Format("update categoria set nombre=@nombre where id=@id");
+
+		IDbDataParameter dbDataParameterNombre = dbCommand.CreateParameter();
+        dbDataParameterNombre.ParameterName = "nombre";
+        dbDataParameterNombre.Value = categoria.Nombre;
+        dbCommand.Parameters.Add(dbDataParameterNombre);
+
+		IDbDataParameter dbDataParameterId = dbCommand.CreateParameter();
+		dbDataParameterId.ParameterName = "id";
+		dbDataParameterId.Value = categoria.Id;
+        dbCommand.Parameters.Add(dbDataParameterId);
+
+        dbCommand.ExecuteNonQuery();
+    }
+
+	private void delete() {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = "delete from categoria where id=4";
+
+        dbCommand.ExecuteNonQuery();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a) {
         Application.Quit();
         a.RetVal = true;
     }
