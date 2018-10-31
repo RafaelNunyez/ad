7d5c791005e9275b2dccd0f1189d30d3aa0f8350
rafaelnunyez <rafa97burja@gmail.com>
2018-10-31 11:31:14 +0100

    Modificado dbprueba.sql (añadida tabla Artículo). Iniciadodo CArticulo

---
+++ CArticulo/CArticulo/ArticuloWindow.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace CArticulo {
+    public partial class ArticuloWindow : Gtk.Window {
+        public ArticuloWindow() :
+                base(Gtk.WindowType.Toplevel) {
+            this.Build();
+        }
+    }
+}
---
+++ CArticulo/CArticulo/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+using Serpis.Ad;
+using Serpis.Ad.Ventas;
+
+public partial class MainWindow : Gtk.Window {
+    public MainWindow() : base(Gtk.WindowType.Toplevel) {
+        Build();
+
+		EntityDao<Articulo> articuloDao = new EntityDao<Articulo>();
+
+		//var list = new[] {
+		//	new { Id = 1, Nombre = "Artículo 1"},
+		//	new { Id = 2, Nombre = "Artículo 2"},
+		//	new { Id = 3, Nombre = "Artículo 3"}
+		//};
+
+		TreeViewHelper.Fill(treeView, new string[] { "Id", "Nombre", "Precio" }, articuloDao.Enumerable);
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a) {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
