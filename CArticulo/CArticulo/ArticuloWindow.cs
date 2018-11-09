5bb4a9df03af1d1846c29f64397793799028ed06
rafaelnunyez <rafa97burja@gmail.com>
2018-11-09 10:35:07 +0100

    EntityDao Load implementado

--- CArticulo/CArticulo/ArticuloWindow.cs
+++ CArticulo/CArticulo/ArticuloWindow.cs
@@ -4,8 +4,7 @@ using Serpis.Ad.Ventas;
 
 namespace CArticulo {
     public partial class ArticuloWindow : Gtk.Window {
-		public ArticuloWindow(Articulo articulo) :
-                base(Gtk.WindowType.Toplevel) {
+		public ArticuloWindow(Articulo articulo) : base(Gtk.WindowType.Toplevel) {
             this.Build();
         }
     }
