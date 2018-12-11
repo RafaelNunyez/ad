8379adfc02b297ffdb9aad91a0a42d5ccd6e011e
rafaelnunyez <rafa97burja@gmail.com>
2018-12-11 08:06:28 +0100

    GCategoria añadidos App y Categoria; iniciado CategoriaDao

--- GCategoria/src/main/java/serpis/ad/CategoriaMain.java
+++ GCategoria/src/main/java/serpis/ad/CategoriaMain.java
@@ -1,13 +1,21 @@
 package serpis.ad;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class CategoriaMain {
-	private static boolean exit = false;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		App.getInstance().setConnection(
+				DriverManager.getConnection("jdbc:mysql://localhost/dbprueba", "root", "sistemas")
+			);
+		
 		Menu.create("Menú Categoria")
 		.exitWhen("\t0 - Salir")
 		.add("\t1 - Nuevo", CategoriaMain::nuevo)
 		.add("\t2 - Editar", CategoriaMain::editar)
+		.add("\t3 - Insertar", CategoriaMain::insertar)
 		.loop();
+		App.getInstance().getConnection().close();
 	}
 	
 	public static void nuevo() {
@@ -17,5 +25,9 @@ public class CategoriaMain {
 	public static void editar() {
 		System.out.println("Método editar");
 		int id = ScannerHelper.getInt("Id: ");
+	}
+	
+	public static void insertar() {
+		System.out.println("Metodo insertar");
 	}
 }
