6b1f95d80adc94c185783fa018091578e0f682ca
rafaelnunyez <rafa97burja@gmail.com>
2019-02-08 09:36:52 +0100

    Añadido HSerpis con App (singleton) y JpaHelper

--- HPedido/src/main/java/serpis/ad/PedidoMain.java
+++ HPedido/src/main/java/serpis/ad/PedidoMain.java
@@ -1,4 +1,5 @@
 package serpis.ad;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,42 +18,24 @@ public class PedidoMain {
 	public static void main(String[] args) {
 		App.getInstance().setEntityManagerFactory(Persistence.createEntityManagerFactory("serpis.ad.hmysql"));
 		
-//		EntityManager entityManager = App.getInstance().getEntityManagerFactory().createEntityManager();
-//		entityManager.getTransaction().begin();
-//	 
-//		List<Categoria> categorias =
-//				entityManager.createQuery("from Categoria", Categoria.class).getResultList();
-//		
-//		Articulo articulo = entityManager.createQuery("from Articulo where id = 12", Articulo.class).getSingleResult();
-//		articulo.setCategoria(categorias.get( new Random().nextInt(categorias.size()) ));
-//		
-//		//entityManager.persist(pruebaArticulo);
-//		
-//		show(articulo);
-//		
-//		update(articulo);
-//		
-//		entityManager.getTransaction().commit();
-//		entityManager.close();
-		
-		List<Categoria> categorias = JpaHelper.execute2(entityManager -> {
+		List<Categoria> categorias = JpaHelper.execute(entityManager -> {
 			return entityManager.createQuery("select c from Categoria c order by id", Categoria.class).getResultList();
 		});
+		
+		for (Categoria categoria : categorias)
+			System.out.printf("%4s %s %n", categoria.getId(), categoria.getNombre());
 		
 		System.out.println("Artículo añadido. Pulse Enter para continuar...");
 		new Scanner(System.in).nextLine();
 		
-		//remove(pruebaArticulo);
-//		doInJPA(entityManagerFactory, entityManager2 -> {
-//			Articulo articulo2 = entityManager2.getReference(Articulo.class, articulo.getId());
-//			entityManager2.remove(articulo2);
-//		});
+		JpaHelper.execute(entityManager -> {
+			Articulo articulo = new Articulo();
+			articulo.setNombre("nuevo " + LocalDateTime.now());
+			articulo.setPrecio(new BigDecimal(1.5));
+			entityManager.persist(articulo);
+		});
 		
-//		Articulo articulo3 = doInJPA(App.getInstance().getEntityManagerFactory(), entityManager -> {
-//			return entityManager.find(Articulo.class, 3L);
-//		});
-		
-		Articulo articulo4 = JpaHelper.execute2(entityManager -> {
+		Articulo articulo4 = JpaHelper.execute(entityManager -> {
 			return entityManager.find(Articulo.class, 3L);
 		});
 		
@@ -60,51 +43,6 @@ public class PedidoMain {
 		
 		App.getInstance().getEntityManagerFactory().close();
 
-	}
-	
-	private static Articulo newArticulo() {
-		Articulo pruebaArticulo = new Articulo();
-		pruebaArticulo.setNombre("prueba " + LocalDateTime.now());
-		pruebaArticulo.setCategoria(null);
-		pruebaArticulo.setPrecio(BigDecimal.valueOf(1));
-		return pruebaArticulo;
-	}
-	
-	/*private static void remove (Articulo articulo) {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		
-		//articulo = entityManager.find(Articulo.class, articulo.getId());
-		articulo = entityManager.getReference(Articulo.class, articulo.getId());
-		
-		entityManager.remove(articulo);
-		
-		entityManager.getTransaction().commit();
-		entityManager.close();
-	}*/
-	
-	/*private static void doInJPA (EntityManagerFactory entityManagerFactory, Consumer<EntityManager> consumer) {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		consumer.accept(entityManager);
-		entityManager.getTransaction().commit();
-		entityManager.close();
-	}
-	
-	private static <R> R doInJPA (EntityManagerFactory entityManagerFactory, Function<EntityManager, R> function) {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		
-		R result = function.apply(entityManager);
-		
-		entityManager.getTransaction().commit();
-		entityManager.close();
-		
-		return result;
-	}*/
-
-	private static void update (Articulo articulo) {
-		articulo.setNombre("prueba 2 " + LocalDateTime.now());
 	}
 	
 	private static void show (Articulo articulo) {
